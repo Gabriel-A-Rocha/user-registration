a467309f4a98d8c7966611f49260e7a98e248988
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2021-04-22 22:51:51 -0400

    feat: build all required routes

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,12 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const userRecord = this.usersRepository.findByEmail(email);
+    if (userRecord) {
+      throw new Error("Email address is already in the database.");
+    }
+    const user = this.usersRepository.create({ name, email });
+    return user;
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,15 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+    if (!user) {
+      throw new Error("User does not exist.");
+    }
+    if (!user.admin) {
+      throw new Error("This action requires admin privileges to be executed.");
+    }
+    const users = this.usersRepository.list();
+    return users;
   }
 }
 
