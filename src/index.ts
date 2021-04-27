9e14e870f40e122e53368b56dfdb06ec8b03e536
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2021-04-26 21:24:43 -0400

    chore: change swagger config file to yaml

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "./swagger.json";
-
+import YAML from "yamljs";
+import { join } from "path";
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
@@ -9,6 +9,9 @@ const app = express();
 app.use(express.json());
 
 app.use("/users", usersRoutes);
+
+const swaggerConfigPath = join(__dirname, "swagger.yml");
+const swaggerDocument = YAML.load(swaggerConfigPath);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
